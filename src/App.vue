2da7b6ab4381e4d07d602dfd9c75647ec35b493d
houxingyi <1026043046@qq.com>
2017-08-16 21:08:06 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-
     <div class="headBar">
       在线点餐（西洪店）
     </div>
-
-    <router-link to="/menu">到菜单</router-link>
-    <router-link to="/seller">到商家</router-link>
-
+    <div class="nav">
+      <div class="nav-item">
+        <router-link class="nav-link" to="/menu">点单</router-link>
+      </div>
+      <div class="nav-item">
+        <router-link class="nav-link" to="/seller">商家</router-link>
+      </div>
+    </div>
     <router-view></router-view> 
   </div>
 </template>
@@ -20,9 +23,29 @@ export default {
 <style lang="scss">
 
   #app{
-
-    
-
+    .headBar{
+      height: 53px;
+      background: #18B4ed;
+      color: #fff;
+      line-height: 53px;
+      text-align: center;
+    }
+    .nav{
+      height: 48px;
+      display: flex;
+      line-height: 48px;
+      .nav-item{
+        flex-grow : 1;
+        text-align: center;
+        .nav-link{
+          display: block;  
+          &.router-link-active{
+            color: #d45604;
+            border-bottom: 3px solid #d45604;
+          }  
+        }
+      }
+    }
   }
 
 
--- src/components/seller/index.vue
+++ src/components/seller/index.vue
@@ -1,6 +1,58 @@
 <template>
   <div id="seller">
-    商家界面
+    <div class="sellerInfo">
+      <div class="logoBox">
+        <img src="../../images/logo.png" alt="logo">
+      </div>
+      <span class="storeName">大泽科技</span>
+      <span class="storeTime">营业时间:8:30-19:00</span>
+      <div class="bottomBar">
+        <div class="left">
+          <span class="price">¥30</span>
+          <span class="priceText">起送价</span>
+        </div>
+        <div class="right">
+          <span class="price">¥30</span>
+          <span class="priceText">起送价</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="loc">
+      <div class="iconBox"></div>
+      <span class="locText">
+        福州市鼓楼区西洪路528号云座-3号楼10层
+      </span>
+      <div class="rigthIcon"></div>
+    </div>
+
+    <div class="payWay">
+      <div class="iconBox"></div>
+      <p class="payText">支持在线支付</p>
+    </div>
+
+    <div class="discounted">
+      <div class="iconBox"></div>
+      <p class="disText">
+        优惠活动<br>
+        部分商品参与买满50减10
+      </p>
+    </div>
+
+
+    <div class="announce">
+      <div class="iconBox"></div>
+      <p class="annText">
+
+        <span class="annTitle">公告</span>
+
+        1.下雨天请提前预订，用餐高峰期延迟送达时间，在线支付便捷。<br>
+        1.下雨天请提前预订，用餐高峰期延迟送达时间，在线支付便捷。
+
+      </p>
+    </div>  
+
+
   </div>
 </template>
 
